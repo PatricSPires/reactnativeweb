7f968545c9e57b9f54e3d814bb7fb9c3def91494
Thiago Gonzalez <contato@thiagogonzalez.com>
2021-01-26 09:50:16 -0500

    remove Switch from App for now

--- packages/commons/src/App.tsx
+++ packages/commons/src/App.tsx
@@ -1,23 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { SafeAreaView, ScrollView, StatusBar } from 'react-native'
-import Switch from './components/Switch'
-
 import { AppHeader } from './AppHeader'
 
 export function App() {
-  const [checked, setChecked] = useState(false)
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView contentInsetAdjustmentBehavior="automatic">
           <AppHeader />
-          <Switch
-            label="Teste"
-            checked={checked}
-            onPress={() => setChecked(!checked)}
-            justifyContent="flex-start"
-          />
         </ScrollView>
       </SafeAreaView>
     </>
