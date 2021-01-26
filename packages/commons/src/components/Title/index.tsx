3957a8182b13b00693910e755ec2079d6ae250ef
Thiago Gonzalez <contato@thiagogonzalez.com>
2021-01-26 14:36:05 -0500

    create a new component called Title

---
+++ packages/commons/src/components/Title/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from 'react-native'
+import styled from 'styled-components'
+
+const Title = styled(Text)`
+  color: red;
+  font-size: 200px;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+export default Title
